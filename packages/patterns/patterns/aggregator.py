ba235fb79343cf518b9e222865191f8e9b6a05a9
Temirlan Ilik <0penguin@mail.ru>
2026-02-25 15:17:22 +0500

    feat:abstract-aggregate

---
+++ packages/patterns/patterns/aggregator.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from typing import List, Iterable
+
+from .message import Event
+
+
+class AbstractAggregate(ABC):
+    """
+    Aggregate - это корневая сущность доменной модели, которая
+    - управляет внутренним состоянием
+    - гарантирует инварианты
+    - генерирует доменные события
+    """
+
+    def __init__(self) -> None:
+        self.events: List[Event] = []
+
+    def _record_event(self, event: Event) -> None:
+        self.events.append(event)
+
+    def pull_events(self) -> Iterable[Event]:
+        event_tpl = tuple(self.events)
+        self.events.clear()
+        return event_tpl
