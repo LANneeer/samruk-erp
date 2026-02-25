c6ecbc7b2fa84e7c84f3b69b25ac3d065ebc8455
Temirlan Ilik <0penguin@mail.ru>
2026-02-25 15:17:36 +0500

    feat:abstract-repository

---
+++ packages/patterns/patterns/repository.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Generic, Optional, TypeVar, Set, Any
+
+from .aggregator import AbstractAggregate
+
+
+T = TypeVar("T", bound=AbstractAggregate)
+
+
+class AbstractRepository(Generic[T], ABC):
+    """
+    Repository - это слой между доменной логикой и базой данных
+    - сохраняет агрегаты
+    - достаёт агрегаты
+    - изолирует доменную модель от ORM
+    """
+
+    def __init__(self) -> None:
+        self.seen: Set[T] = set()
+
+    @abstractmethod
+    def _add(self, aggregate: T) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def _get(self, reference: Any) -> Optional[T]:
+        raise NotImplementedError
+
+    def add(self, aggregate: T) -> None:
+        self._add(aggregate)
+        self.seen.add(aggregate)
+
+    def get(self, reference: Any) -> Optional[T]:
+        agg = self._get(reference)
+        if agg:
+            self.seen.add(agg)
+        return agg
